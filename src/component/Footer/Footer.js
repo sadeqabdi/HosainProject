dd0b371041c9bac811a5043c95eee352ebfecba9
sadeqabdi <abdi.71sadeq@gmail.com>
2020-02-11 11:51:06 +0330

    add Component to project

---
+++ src/component/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react'
+function Footer() {
+  return (
+    <div className="App">
+               <div>
+    <Button content='Primary' primary />
+    <Button content='Secondary' secondary />
+  </div>
+ 
+    </div>
+  );
+}
+
+export default Footer;
